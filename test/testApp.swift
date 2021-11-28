83a6f7a323718fc215a3379c6e9215b9808167f0
jmiltins <miltins.janis@gmail.com>
2021-11-28 20:32:48 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by janis.miltins on 28/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
